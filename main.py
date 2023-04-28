4c0ab06d08d66982362661a4889b389e4c795520
afiqfadzil <afiq.fadzil.99@gmail.com>
2023-04-28 18:25:34 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# This is a sample Python script.
+from kivy.app import App
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.config import Config
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+Builder.load_file('mainscreen.kv')
+Config.set('graphics', 'resizable', False)
+class LoginScreen(Widget):
+    pass
+    '''  def __init__(self, **kwargs):
+       super(LoginScreen,self).__init__(**kwargs)
+        self.add_widget(
+            Button(
+                text="Hello World",
+                size_hint=(.20, .075),
+                pos_hint={'center_x': .2, 'center_y': .10}))
+
+        self.cols = 2
+        self.add_widget(Label(text='Name',pos_hint={'center_x': .2, 'center_y': .75}))
+        self.name = TextInput(multiline=False,size_hint=(.5,.07),pos_hint={'center_x': .5, 'center_y': .75})
+        self.add_widget(self.name)
+        self.add_widget(Label(text='BirthDate', pos_hint={'center_x': .2, 'center_y': .5}))
+        self.birthdate = TextInput(multiline=False, size_hint=(.5, .07), pos_hint={'center_x': .5, 'center_y': .5})
+        self.add_widget(self.birthdate)
+        self.add_widget(Label(text='Height', pos_hint={'center_x': .2, 'center_y': .25}))
+        self.ht = TextInput(multiline=False, size_hint=(.5, .07), pos_hint={'center_x': .5, 'center_y': .25})
+        self.add_widget(self.ht)
+'''
+
+
+class Main(App):
+    def build(self):
+        return LoginScreen()
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    Main().run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
