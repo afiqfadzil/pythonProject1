98754fafa618044af3fe88981f7f62bf523a3987
afiqfadzil <82485677+afiqfadzil@users.noreply.github.com>
2023-05-12 15:47:37 +0900

    Window variable

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.utils import platform
-from kivymd.app import MDApp
+# from kivymd.app import MDApp
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
@@ -12,46 +12,44 @@ from kivy.config import Config
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+test_array = [[], [], [0.5, 0], [0.625, 0], [0.75, 0], [0.875, 0], [1.0, 0], [0.375, 1], [0.5, 1],
+              [0.625, 1], [0.75, 1], [0.875, 1], [1.0, 1], [1.125, 0], [1.25, 0]]
 Builder.load_file("my.kv")
-
-def calculate(self, ht, nt, pre_score):
-    # Calculate Seat Height
-    seat_height = 0.25 * ht - 1
-    print(seat_height)
-    BL = 1
-    OL = 0
-    test_counter = 3
-    test_array = [[], [], [0.5, 0], [0.625, 0], [0.75, 0], [0.875, 0], [1.0, 0], [0.375, 1], [0.5, 1],
-                  [0.625, 1], [0.75, 1], [0.875, 1], [1.0, 1], [1.125, 0], [1.25, 0]]
-    for test_counter in range(3):
-        print(test_array[nt])
-        nt = nt + 2
-
-    '''
+Config.set('graphics', 'resizable', '0')  # 0 being off 1 being on as in true/false
+Config.set('graphics', 'width', '700')
+Config.set('graphics', 'height', '1000')
+Config.write()
+seat_height = 0
 
 
-    nt = 2 [0.5H , 0]
-    nt = 3 [0.625 ,0]
-    nt = 4 [0.75, 0]
-    nt = 5 [0.875 ,0]
-    nt = 6 [1.0,  0]
-    nt = 7 [0.375 ,1]
-    nt = 8 [0.5, 1]
-    nt = 9 [0.625 ,1]
-    nt = 10 [0.75, 1]
-    nt = 11 [0.875, 1]
-    nt = 12 [1.0, 1]
-    nt = 13 [1.125, 0]
-    nt = 14 [1.25, 0]
+class ControlPanel(object):
 
+    def calculate(self):
+        # Calculate Seat Height
+        self.seat_height = 0.25 * self.ht - 1
+        print(self.seat_height)
+        # BothLeg = 1
+        # OneLeg = 0\
+        return self.ht, self.nt, self.seat_height
 
+    def results(self):
+        print("seat height is:", test_array[self.nt])
 
+        return 0
 
+    pass
 
-    '''
 
 class MainWindow(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        MainWindow.ht = 0
+        MainWindow.nt = 0
+        MainWindow.age = 0
+        MainWindow.pre_score = 0
+        MainWindow.seat_height = 0
+
     def data_call(self):
 
         test1_screen = self.manager.get_screen("test")
@@ -78,39 +76,49 @@ class MainWindow(Screen):
             return 0
 
         age = int(age_text)
-        ht = int(ht_text)
+        MainWindow.ht = int(ht_text)
         if age >= 15:
-            nt = 2
+            MainWindow.nt = 2
             pre_score = 95
         elif age >= 35:
-            nt = 4
+            MainWindow.nt = 4
             pre_score = 85
         elif age >= 35:
-            nt = 6
+            MainWindow.nt = 6
             pre_score = 75
         elif age >= 81:
-            nt = 8
+            MainWindow.nt = 8
             pre_score = 65
         elif age < 15 or age > 150:
             main_screen.ids.error_label_main.text = "年齢にエラー (15~150)"
 
             return 0
 
-        calculate(self, ht, nt, pre_score)
         self.manager.current = "test"
         self.manager.transition.direction = "left"
-    pass
 
+
+
+    pass
 
 
 class TestWindow(Screen):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        MainWindow.nt = 0
+
+    def passed(self):
+        # Send Data to python to adjust Seat Height
+        MainWindow.nt = MainWindow.nt + 2
+        print(test_array[MainWindow.nt])
+        return MainWindow.nt
+
+    def failed(self):
+        MainWindow.nt = self.nt - 2
+        return self.nt
+
     pass
-
-
-
-
-
-
 
 
 class ResultWindow(Screen):
@@ -123,6 +131,11 @@ class WindowManager(ScreenManager):
 
 class MyMainApp(App):
     def build(self):
+        if platform == 'android' or platform == 'ios':
+            Window.maximize()
+        else:
+            Window.size = (620, 1024)
+
         return WindowManager()
 
 
