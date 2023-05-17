ad4915fa28cf502914eeba3f26d0a913985422b0
afiqfadzil <afiq.fadzil.99@gmail.com>
2023-05-17 14:55:46 +0900

    FlowChart Completed

--- main.py
+++ main.py
@@ -12,33 +12,21 @@ from kivy.config import Config
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-test_array = [[], [], [0.5, 0], [0.625, 0], [0.75, 0], [0.875, 0], [1.0, 0], [0.375, 1], [0.5, 1],
-              [0.625, 1], [0.75, 1], [0.875, 1], [1.0, 1], [1.125, 0], [1.25, 0]]
+# test_array [SeatHeight,OneL = 0/BothL = 1 , Score , Locomotion Age]
+test_array = [[], [0.5, 0, 100, 16.00], [0.5, 0, 95, 27.95], [0.625, 0, 90, 39.9],
+              [0.75, 0, 85, 51.85], [0.875, 0, 80, 63.8], [1.0, 0, 75, 75.75], [0.375, 1, 70, 87.7],
+              [0.5, 1, 65, 99.65], [0.625, 1, 60, 111.6], [0.75, 1, 55, 123.55], [0.875, 1, 50, 135.5],
+              [1.0, 1, 45, 147.45], [1.125, 0, 40, 159.4], [1.25, 35, 171.35]]
 Builder.load_file("my.kv")
 Config.set('graphics', 'resizable', '0')  # 0 being off 1 being on as in true/false
 Config.set('graphics', 'width', '700')
 Config.set('graphics', 'height', '1000')
 Config.write()
-seat_height = 0
-
-
-class ControlPanel(object):
-
-    def calculate(self):
-        # Calculate Seat Height
-        self.seat_height = 0.25 * self.ht - 1
-        print(self.seat_height)
-        # BothLeg = 1
-        # OneLeg = 0\
-        return self.ht, self.nt, self.seat_height
-
-    def results(self):
-        print("seat height is:", test_array[self.nt])
-
-        return 0
-
-    pass
-
+C = ["素晴らしい基礎運動力です",
+     "良い基礎運動力です",
+     "基礎運動力平均より低い、筋肉バランス感覚のトレーニングをしましょう",
+     "ロコモシンドロームの傾向がありロコモ検診してください",
+     "基礎運動能力がい著しく低下、ちかくの整形外科病院で受診してください"]
 
 class MainWindow(Screen):
     def __init__(self, **kwargs):
@@ -49,6 +37,12 @@ class MainWindow(Screen):
         MainWindow.age = 0
         MainWindow.pre_score = 0
         MainWindow.seat_height = 0
+
+    def seat(self):
+        # Calculate Seat Height
+        MainWindow.seat_height = 0.25 * MainWindow.ht - 1
+        print(MainWindow.seat_height)
+        return 0
 
     def data_call(self):
 
@@ -75,48 +69,138 @@ class MainWindow(Screen):
             main_screen.ids.error_label_main.text = "年齢を入力してください！"
             return 0
 
-        age = int(age_text)
+        MainWindow.age = int(age_text)
         MainWindow.ht = int(ht_text)
-        if age >= 15:
-            MainWindow.nt = 2
-            pre_score = 95
-        elif age >= 35:
-            MainWindow.nt = 4
-            pre_score = 85
-        elif age >= 35:
-            MainWindow.nt = 6
-            pre_score = 75
-        elif age >= 81:
+        if MainWindow.age < 15 or MainWindow.age > 150:
+            main_screen.ids.error_label_main.text = "年齢にエラー (15~150)"
+            return 0
+        elif MainWindow.age >= 81:
             MainWindow.nt = 8
             pre_score = 65
-        elif age < 15 or age > 150:
-            main_screen.ids.error_label_main.text = "年齢にエラー (15~150)"
+        elif MainWindow.age >= 35:
+            MainWindow.nt = 6
+            pre_score = 75
+        elif MainWindow.age >= 35:
+            MainWindow.nt = 4
+            pre_score = 85
 
-            return 0
-
+        elif MainWindow.age >= 15:
+            MainWindow.nt = 2
+            pre_score = 95
+        print(MainWindow.nt)
         self.manager.current = "test"
         self.manager.transition.direction = "left"
-
-
 
     pass
 
 
 class TestWindow(Screen):
+    # R = MainWindow.age - test_array[MainWindow.nt][3]
 
     def __init__(self, **kw):
         super().__init__(**kw)
-        MainWindow.nt = 0
+        TestWindow.c = 0
+        TestWindow.score = 0
+        self.prev = 0
+        self.rating = 0
+
+    def rating_func(self):
+        if TestWindow.c == 3:
+            self.rating = MainWindow.age - test_array[MainWindow.nt][3]
+
+            if int(self.rating) > 20:
+                print(C[0])
+                pass
+
+            elif int(self.rating) > 0:
+                print(C[1])
+                pass
+            elif int(self.rating) > (-20):
+                print(C[2])
+                pass
+            elif int(self.rating) > (-30):
+                print(C[3])
+                pass
+            elif int(self.rating) < -30:
+                print(C[4])
+                pass
+            else:
+                pass
+
+        pass
+
+    def next_screen(self):
+        if TestWindow.c == 4:
+            self.manager.current = "result"
+            self.manager.transition.direction = "left"
+            TestWindow.c = 0
 
     def passed(self):
-        # Send Data to python to adjust Seat Height
-        MainWindow.nt = MainWindow.nt + 2
-        print(test_array[MainWindow.nt])
+        if TestWindow.c < 2:
+            if TestWindow.c == 0:  # Pass
+                # Send Data to python to adjust Seat Height
+                MainWindow.nt = MainWindow.nt - 2
+                # if nt score is lower than 1 set MainWindow.nt == 1
+
+                if MainWindow.nt < 1:
+                    MainWindow.nt = 1
+
+
+            elif TestWindow.c == 1 and int(self.prev) == 0:  # PassPass
+                MainWindow.nt = MainWindow.nt - 2
+                if MainWindow.nt < 1:
+                    MainWindow.nt = 1
+
+
+            elif TestWindow.c == 1 and int(self.prev) == 1:  # FailPass
+                MainWindow.nt = MainWindow.nt - 1
+                if MainWindow.nt < 1:
+                    MainWindow.nt = 1
+            else:
+                pass
+
+        if TestWindow.c == 2:  # FinalMarking
+            self.rating = float(test_array[MainWindow.nt][3]) - float(MainWindow.age)
+            print(self.rating)
+
+        self.prev = 0
+        TestWindow.c = TestWindow.c + 1
+        print(MainWindow.nt)
         return MainWindow.nt
 
     def failed(self):
-        MainWindow.nt = self.nt - 2
-        return self.nt
+        if TestWindow.c < 2:
+
+            if TestWindow.c == 0:  # Pass
+                MainWindow.nt = MainWindow.nt + 2
+                if MainWindow.nt > 14:
+                    MainWindow.nt = 14
+
+            elif TestWindow.c == 1 and int(self.prev) == 1:  # FailFail
+                MainWindow.nt = MainWindow.nt + 2
+                if MainWindow.nt > 14:
+                    MainWindow.nt = 14
+
+                print("FailFail")
+
+
+            elif TestWindow.c == 1 and int(self.prev) == 0:  # PassFail
+                MainWindow.nt = MainWindow.nt + 1
+                if MainWindow.nt > 14:
+                    MainWindow.nt = 14
+                print("PassFail")
+
+        if TestWindow.c == 2:  # FinalMarking
+            MainWindow.nt = MainWindow.nt + 1
+            self.rating = float(MainWindow.age) - float(test_array[MainWindow.nt][3])
+
+            print(self.rating)
+            pass
+
+        self.prev = 1
+        TestWindow.c = TestWindow.c + 1
+
+        return MainWindow.nt
 
     pass
 
